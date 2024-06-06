3af1c2e57086d3ccf3e33937ac6a0bfd77e101fa
kacperDebowski <167117372+kacperDebowski@users.noreply.github.com>
2024-06-06 22:38:09 +0200

    aktualizacja 06.06 22:38

--- level.gd
+++ level.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 var direction = 0
 var alive = true
 var healthPoints = 100
+var attacks = false
 @onready var deadLabel = $CanvasLayer/deadLabel
+@onready var winLabel = $CanvasLayer/winLabel
 
 func _ready():
-	$AnimatedSprite2D.play("idle")
+	if not attacks:
+		$AnimatedSprite2D.play("idle")
 
 func _process(float):
 	if alive: 
@@ -22,7 +25,7 @@ func _process(float):
 		elif Input.is_action_pressed("down"):
 			$AnimatedSprite2D.play("run")
 			velocity.y = 300
-		else:
+		else: 
 			velocity = Vector2.ZERO
 			$AnimatedSprite2D.play("idle")
 			
@@ -36,24 +39,38 @@ func _process(float):
 		if Input.is_action_just_pressed("use"):
 			healing()	
 			
+		if Input.is_action_pressed("useAttack"):
+			attack()
+			
 		move_and_slide()
 			
 	if Input.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 		
 func dead():
-		alive = false
-		$AnimatedSprite2D.play("dead")
-		deadLabel.visible = true
+	alive = false
+	$AnimatedSprite2D.play("dead")
+	deadLabel.visible = true
+		
+func win():
+	winLabel.visible = true
 		
 func healing():
 	if healthPoints < 100:
 		healthPoints += Global.potionHealthRestored
 		updateHealthPoints()
+		if healthPoints > 100:
+			healthPoints = 100
+			updateHealthPoints()
 		Global.collectedPotions -= 1
 		updatePotionCount()
 	elif healthPoints >= 100:
 		pass
+		
+func attack():
+	attacks = true
+	$AnimatedSprite2D.play("slash")
+	attacks = false
 			
 @onready var HealthPoints = $CanvasLayer/PanelContainer/MarginContainer/HBoxContainer/HealthInfo/HealtchPoints
 func updateHealthPoints():
